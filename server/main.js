8092ccd35b7dc7109f5b6fad828aee8c49b1820e
Hugo Gonzalez <thegonzalezdesigns@gmail.com>
2016-07-28 14:20:22 -0700

    Imported passport.

--- server/main.js
+++ server/main.js
@@ -1,4 +1,5 @@
-import { Meteor } from 'meteor/meteor';
+import { Meteor }		from 'meteor/meteor';
+import { passport }		from 'passport'
 
 Meteor.startup(() => {
   // code to run on server at startup
