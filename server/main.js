907ebd00d0d58222daf2f38e84ce756ab183b039
Hugo Gonzalez <thegonzalezdesigns@gmail.com>
2016-08-04 19:08:00 -0700

    Semantics

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
+/*jshint esnext: true, browser: true*/
 import { Meteor } from 'meteor/meteor';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+	// code to run on server at startup
 });
