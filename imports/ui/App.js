74508974bd0f42911ef6f54f51c3b7679be12fc1
Hugo Gonzalez <thegonzalezdesigns@gmail.com>
2016-08-04 19:10:19 -0700

    Main application component.

---
+++ imports/ui/App.js
@@ -0,0 +1,29 @@
+/*jshint esnext: true, browser: true, indent: 4*/
+import React, { Component, PropTypes } from 'react';
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { createContainer } from 'meteor/react-meteor-data';
+import AccountsUIWrapper from './AccountsUIWrapper.js';
+
+// App component - represents the whole app
+class App extends Component {
+	render() {
+		return (
+			<div className="container">
+				<header>
+					<AccountsUIWrapper />
+				</header>
+			</div>
+		);
+	}
+}
+
+App.propTypes = {
+	currentUser: PropTypes.object,
+};
+
+export default createContainer(() => {
+	return {
+		currentUser: Meteor.user(),
+	};
+}, App);
