f01c05e93544aa225fe9b4a4080d09aadca26776
Han Koh <rhgksrua@gmail.com>
2016-08-13 23:30:17 -0500

    fixed packages

--- imports/startup/accounts-config.js
+++ imports/startup/accounts-config.js
@@ -4,5 +4,9 @@ import { Accounts } from "meteor/accounts-base";
  * Without this file the sign up form will default to ask for an email instead.
  */
 Accounts.ui.config({
-	passwordSignupFields: "USERNAME_ONLY"
+    passwordSignupFields: "USERNAME_ONLY",
+});
+
+Accounts.config({
+    forbidClientAccountCreation: false,
 });
